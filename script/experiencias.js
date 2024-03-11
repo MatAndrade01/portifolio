40084c31125c7c062091ce51adec63ace263978e
Matheus Andrade <125470760+MatAndrade01@users.noreply.github.com>
2024-03-11 09:22:32 -0300

    Finalizando a tela para celular

---
+++ script/experiencias.js
@@ -0,0 +1,25 @@
+function clickMenu() {
+    if (Menu.style.display == 'block') {
+        Menu.style.display = 'none'
+    }
+
+    else {
+        Menu.style.display = 'block'
+        burguer.style.display = 'none'
+        document.body.style.overflowY = 'hidden';
+        topbar.style.height = '100vh'
+    }
+}
+
+function clickArrow() {
+    if (Menu.style.display == 'block') {
+        Menu.style.display = 'none'
+    }
+
+    if(Menu.style.display == 'none') {
+        burguer.style.display = 'block'
+        experiencias.style.display = 'block'
+        document.body.style.overflowY = 'auto';
+        topbar.style.height = '34px'
+    }
+}
